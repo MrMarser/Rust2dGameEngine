38b79e147d19cd7f74314fe5a793114bd503a420
Nikita Guzarskii <mr.marser007@gmail.com>
2024-11-13 05:24:24 +0300

    main

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    window::WindowBuilder,
+    dpi::PhysicalSize,
+};
+use winit::event_loop::ControlFlow;
+
+fn main() {
+
+    let event_loop = EventLoop::new();
+
+    let window_size: PhysicalSize<u32> = (640, 480).into();
+
+    let _window = WindowBuilder::new()
+        .with_fullscreen(None)
+        .with_inner_size(window_size)
+        .with_title("MarsEngine")
+        .build(&event_loop)
+        .unwrap();
+
+
+    event_loop.run(move |event, _, control_flow| {
+
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::MainEventsCleared => {
+
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => {}
+        }
+
+    })
+}
